8af50ea3df9c0fc574b67ba0624cf1a18ff8337a
ijungleboy <daniel.mettler@2sic.com>
2021-09-23 16:25:39 +0200

    lot of minor stuff

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,9 +2,9 @@ let BeerSlider = require('../../node_modules/beerslider');
 require('../scss/_style.scss');
 
 const winAny = (window as any);
-winAny.appImageCompare2 = winAny.appImageCompare2 || {};
-winAny.appImageCompare2.init = winAny.appImageCompare2.init || init;
+winAny.appImageCompare2 ??= {};
+winAny.appImageCompare2.init ??= init;
 
-export function init({ moduleId } : { moduleId: string }) {
-    new BeerSlider(document.getElementById(`beerslider-${moduleId}`));
+export function init({ domId } : { domId: string }) {
+    new BeerSlider(document.getElementById(domId));
 }
